ee09ea60abb2ae28caedd2719fe0f2a9cef7f8ee
Christian De La Cruz <delacruzfamilia13@gmail.com>
2021-07-18 17:24:59 -0400

    [IMP] Markdown viewer in description

--- components/api-descriotion.tsx
+++ components/api-descriotion.tsx
@@ -1,12 +1,19 @@
 import React, { useContext } from 'react';
 import { LanguageContext } from '../context/language.context';
+import Markdown from 'react-markdown';
 
-export default function ApiDescription({ description }: any) {
+export default function ApiDescription({
+  description,
+}: {
+  description: string;
+}) {
   const { t } = useContext<any>(LanguageContext);
   return (
     <section className="card">
       <h2 className="text-xl mb-4">{t.apiDetails.apiDescription}</h2>
-      <p>{description}</p>
+      <article>
+        <Markdown>{description}</Markdown>
+      </article>
     </section>
   );
 }
