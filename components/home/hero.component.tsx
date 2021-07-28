1f13d4fc6816415faae7771f07c89ee41552b384
Christian De La Cruz <delacruzfamilia13@gmail.com>
2021-07-27 23:39:58 -0400

    [ADD] Poppins font

--- components/home/hero.component.tsx
+++ components/home/hero.component.tsx
@@ -8,16 +8,21 @@ export default function HeroComponent() {
   return (
     <section className="bg-blue-primary-light flex justify-center md:justify-between  items-center px-6 py-12">
       <div className="hidden md:block">
+      <Image
+          src="/images/hero-illustration.svg"
+          width={350}
+          height={350}
+          alt="Hero illustration"
+        />
+      </div>
+      <div className="text-gray-800 flex flex-col items-start w-full md:w-2/5">
         <Image
           src="/images/ogtic-full-logo.png"
           width="200px"
           height="74px"
           alt="OGTIC logo"
         />
-      </div>
-      <div className="text-gray-800 flex flex-col items-start w-full md:w-2/5">
-        <h3 className="text-2xl mb-4">{t.hero.ctaTitle}</h3>
-        <p className="mb-4">{t.hero.ctaDescription}</p>
+        <p className="my-6 font-medium text-2xl">{t.hero.ctaDescription}</p>
         <Link href="/apis">
           <a className="bg-green-600 px-4 py-2 rounded-md shadow-md">
             {t.hero.ctaButton}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,18 @@ import 'tailwindcss/tailwind.css';
 import '../styles/globals.css';
 import Layout from '../components/layout.component';
 import LanguageProvider from '../context/language.context';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <LanguageProvider>
+      <Head>
+        <title>Test</title>
+        <link
+          href="https://fonts.googleapis.com/css?family=Poppins"
+          rel="stylesheet"
+        />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
