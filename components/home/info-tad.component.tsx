076b3eddfe105d98e0ec615f90f6947423ef6159
Christian De La Cruz <delacruzfamilia13@gmail.com>
2021-07-28 23:42:43 -0400

    [IMP] Info tad design

--- components/home/info-tad.component.tsx
+++ components/home/info-tad.component.tsx
@@ -5,11 +5,11 @@ export default function InfoTad() {
   const { t } = useContext<any>(LanguageContext);
 
   return (
-    <div className="container mx-auto flex my-20">
-      <p className="w-1/2 flex items-center text-3xl px-36 font-medium">
+    <div className="container mx-auto flex my-20 flex-col md:flex-row px-10">
+      <p className="w-full md:w-1/2 flex items-center text-3xl md:px-28 font-medium mb-8">
         {t.infoTad.title}
       </p>
-      <p className="w-1/2 flex items-center">{t.infoTad.info}</p>
+      <p className="w-full md:w-1/2 flex items-center">{t.infoTad.info}</p>
     </div>
   );
 }
