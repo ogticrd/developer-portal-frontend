e4fb3bb9ce029815ed468d9bbbc8fb895a407683
Christian De la Cruz <delacruzfamilia13@gmail.com>
2021-08-27 09:12:56 -0400

    [FIX] Linting errors

--- next.config.js
+++ next.config.js
@@ -7,4 +7,4 @@ module.exports = {
   images: {
     domains: ['developers.digital.gob.do', 'localhost'],
   },
-};
+}
--- public/locales/en.js
+++ public/locales/en.js
@@ -72,4 +72,4 @@ export const en = {
     contact: 'Contact',
     follow: 'Follow us',
   },
-};
+}
