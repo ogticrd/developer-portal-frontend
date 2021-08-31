fc6cc3bcb69dddb6c4bee7e26b5127754959ff1a
Christian De La Cruz <delacruzfamilia13@gmail.com>
2021-08-30 21:39:17 -0400

    [FIX] Show api details on profile

--- components/apis/api-page.tsx
+++ components/apis/api-page.tsx
@@ -1,41 +1,26 @@
-import { useContext, useEffect, useState } from 'react';
-import { ApiPage } from '../../models/api-pages';
-import { getPages, getPageContent } from '../../services/apis.service';
-import SwaggerUI from 'swagger-ui-react';
-import 'swagger-ui-react/swagger-ui.css';
-import { LanguageContext } from '../../context/language.context';
-import ApiVersionTag from './api-version';
-import MarkdownViewer from './markdown-viewer';
+import { useContext, useEffect, useState } from 'react'
+import { ApiPage } from '../../models/api-pages'
+
+import SwaggerUI from 'swagger-ui-react'
+import 'swagger-ui-react/swagger-ui.css'
+import { LanguageContext } from '../../context/language.context'
+import ApiVersionTag from './api-version'
+import MarkdownViewer from './markdown-viewer'
 
 export default function ApiPageComponent({
-  id,
   version,
+  swaggerContent,
+  markdownContent,
 }: {
-  id: string;
-  version: string;
+  version: string
+  swaggerContent: ApiPage
+  markdownContent: any
 }): JSX.Element {
-  const [swaggerPage, setSwaggerPage] = useState<ApiPage>();
-  const [markdownPage, setMarkdownPage] = useState<string>();
-
-  const { t } = useContext<any>(LanguageContext);
-
-  useEffect(() => {
-    const getPagesInfo = async () => {
-      const { swagger, markdown } = await getPages(id);
-      setSwaggerPage(swagger);
-      if (markdown) {
-        const content = await getPageContent(markdown._links.content);
-        setMarkdownPage(content);
-      }
-    };
-
-    getPagesInfo();
-    return () => {};
-  }, [id]);
+  const { t } = useContext<any>(LanguageContext)
 
   return (
     <>
-      {markdownPage && (
+      {markdownContent && (
         <section id="documentation" className="card">
           <div className="flex items-center mb-4">
             <h2 className="text-2xl font-semibold">
@@ -44,21 +29,21 @@ export default function ApiPageComponent({
             <ApiVersionTag version={version} />
           </div>
           <hr className="mb-3" />
-          <MarkdownViewer content={markdownPage} />
+          <MarkdownViewer content={markdownContent} />
         </section>
       )}
 
-      {swaggerPage && (
+      {swaggerContent && (
         <section id="api" className="card w-full overflow-x-auto">
           <div
             style={{
               minWidth: 500,
             }}
           >
-            <SwaggerUI url={swaggerPage?._links.content} />
+            <SwaggerUI url={swaggerContent?._links.content} />
           </div>
         </section>
       )}
     </>
-  );
+  )
 }
