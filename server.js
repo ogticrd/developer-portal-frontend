27ed306b1fc5bdca74ecd5e02db942ad9255e8ea
Christian De La Cruz <delacruzfamilia13@gmail.com>
2021-10-04 12:38:28 -0400

    [TEST] PORT 80 And not default port on server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const next = require('next')
 const axios = require('axios')
 require('dotenv').config()
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT
 const dev = process.env.NODE_ENV !== 'production'
 const apiUrl = process.env.REACT_APP_API_URL
 
