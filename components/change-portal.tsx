7f95c237778b2d06f58f050bbb9a5127421acf28
Christian De La Cruz <delacruzfamilia13@gmail.com>
2022-03-09 11:41:31 -0400

    [ADD] Link to design portal

--- components/change-portal.tsx
+++ components/change-portal.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React, { useContext } from 'react';
 import { LanguageContext } from '../context/language.context';
 import LanguageToggle from './language-toggle.component';
@@ -6,9 +7,11 @@ export default function ChangePortal() {
     const { t } = useContext<any>(LanguageContext)
 
     return <div className='flex gap-8 text-white topbar-right mt-8 md:mt-0 mx-auto md:mx-0 font-medium text-sm'>
-        <span className="portal-inactive">
-            <p className='py-4 flex items-center'>{t.header.menu.designers}</p>
-        </span>
+        <Link href={'https://uxkit.digital.gob.do'}>
+            <a className="portal-inactive">
+                <p className='py-4 flex items-center'>{t.header.menu.designers}</p>
+            </a>
+        </Link>
         <span className="portal-active">
             <p className='py-4 flex items-center'>{t.header.menu.developers}</p>
         </span>
